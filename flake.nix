4090495a1e47cd702bfe9378f0b2f0cc07f95590
Eliud niebles <eliudnibles@gmail.com>
2025-02-16 17:13:07 -0500

    Configuracion de lazy, lsp y mason  inicial

--- flake.nix
+++ flake.nix
@@ -80,6 +80,7 @@
       # this includes LSPs
       lspsAndRuntimeDeps = {
         general = with pkgs; [
+          lua-language-server
         ];
       };
 
@@ -92,8 +93,13 @@
       # not loaded automatically at startup.
       # use with packadd and an autocommand in config to achieve lazy loading
       optionalPlugins = {
-        gitPlugins = with pkgs.neovimPlugins; [ ];
-        general = with pkgs.vimPlugins; [ ];
+        # gitPlugins = with pkgs.neovimPlugins; [ ];
+        general = with pkgs.vimPlugins; [ 
+          vim-sleuth
+          nvim-lspconfig
+          fidget-nvim
+          lazydev-nvim
+        ];
       };
 
       # shared libraries to be added to LD_LIBRARY_PATH
---
+++ init.lua
@@ -0,0 +1,39 @@
+require("nixCatsUtils").setup({
+  non_nix_value = true,
+})
+
+-- NOTE: nixCats: You might want to move the lazy-lock.json file
+local function getlockfilepath()
+  if require("nixCatsUtils").isNixCats and type(nixCats.settings.unwrappedCfgPath) == "string" then
+    return nixCats.settings.unwrappedCfgPath .. "/lazy-lock.json"
+  else
+    return vim.fn.stdpath("config") .. "/lazy-lock.json"
+  end
+end
+local lazyOptions = {
+  lockfile = getlockfilepath(),
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = "⌘",
+      config = "🛠",
+      event = "📅",
+      ft = "📂",
+      init = "⚙",
+      keys = "🗝",
+      plugin = "🔌",
+      runtime = "💻",
+      require = "🌙",
+      source = "📄",
+      start = "🚀",
+      task = "📌",
+      lazy = "💤 ",
+    },
+  },
+}
+require("nixCatsUtils.lazyCat").setup(nixCats.pawsible({ "allPlugins", "start", "lazy.nvim" }), {
+  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+  { import = "custom.plugins" },
+}, lazyOptions)
