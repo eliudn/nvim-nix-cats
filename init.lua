bc3ffbb6ed90bc26af5787b3ec4a1cccae3baa00
Eliud niebles <eliudnibles@gmail.com>
2025-02-21 14:46:38 -0500

    se agrega snacks y lazygit

--- init.lua
+++ init.lua
@@ -2,6 +2,34 @@ require("nixCatsUtils").setup({
   non_nix_value = true,
 })
 
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+vim.o.splitbelow = true
+vim.o.splitright = true
+vim.o.mouse = "";
+vim.o.expandtab = true;
+vim.o.tabstop = 4;
+vim.o.softtabstop = 4;
+vim.o.shiftwidth = 4;
+vim.o.smartindent = true;
+vim.o.errorbells = false;
+vim.o.wrap = false;
+vim.o.swapfile = false;
+vim.o.undofile = true;
+vim.o.hlsearch = false;
+vim.o.incsearch = true;
+vim.o.ignorecase = true;
+vim.o.smartcase = true;
+vim.o.colorcolumn = "80";
+vim.o.termguicolors = true;
+vim.o.sidescrolloff = 4;
+vim.o.showmode = false;
+vim.o.clipboard = "unnamedplus";
+
 -- NOTE: nixCats: You might want to move the lazy-lock.json file
 local function getlockfilepath()
   if require("nixCatsUtils").isNixCats and type(nixCats.settings.unwrappedCfgPath) == "string" then
@@ -39,5 +67,7 @@ require("nixCatsUtils.lazyCat").setup(nixCats.pawsible({ "allPlugins", "start", 
   require("core.treesitter"),
   require("core.file_manager"),
   require("core.fuzzy_finder"),
+  require("core.completacion"),
+
   { import = "custom.plugins" },
 }, lazyOptions)
--- lua/core/file_manager.lua
+++ lua/core/file_manager.lua
@@ -1,9 +1,14 @@
 return {
-  "stevearc/oil.nvim",
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  enabled = require("nixCatsUtils").enableForCategory("file-manager"),
-  cmd = { "Oil" },
-  dependencies = {{"echasnovski/mini.icons",opts = {}}},
+	"stevearc/oil.nvim",
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {},
+	enabled = require("nixCatsUtils").enableForCategory("file-manager"),
+	keys = {
+		{ "-", "<cmd>Oil<CR>" },
+		{ "<C-->", function() require("oil").toggle_float() end},
+    { "|", function () require("oil").open(nil,{preview = {vertical = true}}) end }
+	},
+	cmd = { "Oil" },
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
 }
