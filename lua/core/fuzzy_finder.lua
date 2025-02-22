e047d3eb1e5288c0a18a107b1983faae950dfab5
Eliud niebles <eliudnibles@gmail.com>
2025-02-21 19:43:17 -0500

    se agrega los keymap para snack

--- lua/core/fuzzy_finder.lua
+++ lua/core/fuzzy_finder.lua
@@ -9,6 +9,7 @@ return {
     vim.keymap.set({"n"}, "<leader>pp", require('fzf-lua').git_files )
     vim.keymap.set({"n"}, "<leader>pe", require('fzf-lua').buffers )
     vim.keymap.set({"n"}, "<leader>pl", require('fzf-lua').live_grep )
-    vim.keymap.set({"n"}, "<leader>ph", require('fzf-lua').helptags )
+    -- vim.keymap.set({"n"}, "<leader>ph", require('fzf-lua').helptags )
+
   end
 }
